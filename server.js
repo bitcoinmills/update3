bb8881b969833db11182e9eb7f547dd7325e24bf
ju3tin <ju3tin@hotmail.co.uk>
2022-11-09 02:31:54 +0000

    update

--- server.js
+++ server.js
@@ -919,40 +919,41 @@ app.post('/:user/signature', (req, res) => {
   });
 });
 
-
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+  if (!err && count === 0) {
+  new Role({
+  name: "user"
+  }).save(err => {
+  if (err) {
+  console.log("error", err);
+  }
+  console.log("added 'user' to roles collection");
+  });
+  new Role({
+  name: "moderator"
+  }).save(err => {
+  if (err) {
+  console.log("error", err);
+  }
+  console.log("added 'moderator' to roles collection");
+  });
+  new Role({
+  name: "admin"
+  }).save(err => {
+  if (err) {
+  console.log("error", err);
+  }
+  console.log("added 'admin' to roles collection");
+  });
+  }
+  });
+}
 
 //------------------------------------------------------------------------------
 //end of stripe
+
 app.listen(PORT, () => {
 console.log(`Server is running on port ${PORT}.`);
 });
-function initial() {
-Role.estimatedDocumentCount((err, count) => {
-if (!err && count === 0) {
-new Role({
-name: "user"
-}).save(err => {
-if (err) {
-console.log("error", err);
-}
-console.log("added 'user' to roles collection");
-});
-new Role({
-name: "moderator"
-}).save(err => {
-if (err) {
-console.log("error", err);
-}
-console.log("added 'moderator' to roles collection");
-});
-new Role({
-name: "admin"
-}).save(err => {
-if (err) {
-console.log("error", err);
-}
-console.log("added 'admin' to roles collection");
-});
-}
-});
-}
+
